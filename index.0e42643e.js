c6828d2fc6a6848043cc45c44346ef786eb095b0
Anna <ania.somko@gmail.com>
2024-07-08 17:35:45 +0300

    make build

---
+++ index.0e42643e.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall");e.style.left=(t.offsetWidth-e.offsetWidth)/2+"px",e.style.top=(t.offsetHeight-e.offsetHeight)/2+"px";
+//# sourceMappingURL=index.0e42643e.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,8 @@
+"use strict";
+// write code here
+const spider = document.querySelector(".spider");
+const wall = document.querySelector(".wall");
+spider.style.left = (wall.offsetWidth - spider.offsetWidth) / 2 + "px";
+spider.style.top = (wall.offsetHeight - spider.offsetHeight) / 2 + "px";
+
+//# sourceMappingURL=index.f75de5e1.js.map
